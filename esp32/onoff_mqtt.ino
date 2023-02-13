16e56ffcae8de82f6c38063c975d568226046fad
brojackvn <64636218+brojackvn@users.noreply.github.com>
2023-02-13 10:21:48 +0700

    Initial Prject

---
+++ esp32/onoff_mqtt.ino
@@ -0,0 +1,87 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "267 Kham Thien Tang 3";
+const char* password = "khamthient3";
+
+#define MQTT_SERVER "broker.hivemq.com"
+#define MQTT_PORT 1883
+#define MQTT_USER "brojackvn"
+#define MQTT_PASSWORD "brojackvn"
+#define MQTT_LDP_TOPIC "iotlab"
+
+int current_ledState = LOW;
+int last_ledState = LOW;
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+void setup_wifi() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void connect_to_broker() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "IoTLab4";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println("connected");
+      client.subscribe(MQTT_LDP_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 2 seconds");
+      delay(2000);
+    }
+  }
+}
+
+void callback(char* topic, byte *payload, unsigned int length) {
+  Serial.println("-------new message from broker-----");
+  Serial.print("topic: ");
+  Serial.println(topic);
+  Serial.print("message: ");
+  Serial.write(payload, length);
+  Serial.println();
+  if (*payload == '1') current_ledState = HIGH;
+  if (*payload == '0') current_ledState = LOW;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(500);
+  setup_wifi();
+  client.setServer(MQTT_SERVER, MQTT_PORT );
+  client.setCallback(callback);
+  connect_to_broker();
+}
+
+void send_data() {
+  client.publish(MQTT_LDP_TOPIC, "brojackvn hello world"); 
+  delay(1000);
+}
+
+void loop() {
+  client.loop();
+  if (!client.connected()) {
+    connect_to_broker();
+  }
+  if (last_ledState != current_ledState) {
+    last_ledState = current_ledState;
+    digitalWrite(22, current_ledState);
+    Serial.println(current_ledState);
+  }
+  // send_data();
+}
